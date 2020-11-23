d6e4cbe0c13e1763719cfdd4e1e083c8f281e442
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-23 18:15:53 +0530

    updated 1.2 pattern 18 soln

--- Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-18-official.java
+++ Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-18-official.java
@@ -1,1 +1,34 @@
-esign-pattern-18-official
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int st = n;
+        int sp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < sp; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j < st; j++) {
+                if (((i > 0) && (i < n / 2)) && ((j > 0) && (j < st - 1))) {
+                    System.out.print("\t");
+                } else {
+                    System.out.print("*\t");
+                }
+            }
+            if (i < n / 2) {
+                sp++;
+                st -= 2;
+            } else {
+                sp--;
+                st += 2;
+            }
+            System.out.println();
+        }
+
+    }
+}
