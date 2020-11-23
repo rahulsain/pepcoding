bfe399e330efbf5f877aa8b5660fdc919fe217b0
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-23 18:49:45 +0530

    updated soln of 1.2 pattern 19

--- Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-19-official.java
+++ Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-19-official.java
@@ -1,1 +1,33 @@
-design-pattern-19-official
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == mid || j == mid) {
+                    System.out.print("*\t");
+                } else {
+                    if (i == 0 && j < mid) {
+                        System.out.print("*\t");
+                    } else if (j == n - 1 && i < mid) {
+                        System.out.print("*\t");
+                    } else if (i == n - 1 && j > mid) {
+                        System.out.print("*\t");
+                    } else if (j == 0 && i > mid) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
