f560869eae9e5886c7b54554c4c02c8b3ae90220
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-23 10:42:50 +0530

    added more 1.2 pattern soln upto pattern 8

--- Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-6-official.java
+++ Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-6-official.java
@@ -5,7 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        // write ur code here
+        int n = scn.nextInt();
+        int mid = n / 2 + 1;
 
+        for (int i = 0; i < mid; i++) {
+            for (int j = i; j < mid; j++) {
+                System.out.print("*\t");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("\t");
+            }
+            for (int j = i; j < mid; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+        for (int i = mid -1; i > 0; i--) {
+            for (int j = i; j <= mid; j++) {
+                System.out.print("*\t");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("\t");
+            }
+            for (int j = i; j <= mid; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+        scn.close();
     }
 }
