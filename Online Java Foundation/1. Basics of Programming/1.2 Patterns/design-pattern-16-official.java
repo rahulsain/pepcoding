80826fcb01921206c5cec6c8f4f44c1ccf0653c2
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-23 17:15:25 +0530

    updated 1.2 pattern 16 soln

--- Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-16-official.java
+++ Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-16-official.java
@@ -1,1 +1,32 @@
-design-pattern-16-official
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int sp = 2 * n - 3;
+
+        for (int i = 0; i < n; i++) {
+            int temp = 0;
+            int stp = sp;
+            for (int j = 0; j < 2 * n - 1; j++) {
+                if (j <= i) {
+                    System.out.print(++temp + "\t");
+                } else if (stp > 0) {
+                    System.out.print("\t");
+                    stp--;
+                } else {
+                    if (temp == n) {
+                        temp--;
+                    }
+                    System.out.print(temp-- + "\t");
+                }
+            }
+            sp -= 2;
+            System.out.println();
+        }
+
+    }
+}
