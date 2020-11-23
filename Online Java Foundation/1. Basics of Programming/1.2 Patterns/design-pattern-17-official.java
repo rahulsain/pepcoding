6939630ba93a8bc0a5f00a0a1508f624906d42f6
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-23 17:46:05 +0530

    updated soln of 1.2 pattern 17

--- Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-17-official.java
+++ Online Java Foundation/1. Basics of Programming/1.2 Patterns/design-pattern-17-official.java
@@ -1,1 +1,39 @@
-design-pattern-17-official
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int sp = n / 2;
+        int temp = 1;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < sp; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j < temp; j++) {
+
+                System.out.print("*\t");
+
+            }
+            if (temp == n) {
+                temp = n / 2 + 1;
+            }
+            if (i < (n / 2) - 1) {
+                temp++;
+                sp = n / 2;
+            } else if (i == (n / 2) - 1) {
+                temp = n;
+                sp = 0;
+            } else {
+                temp--;
+                sp = n / 2;
+            }
+            System.out.println();
+        }
+
+    }
+}
