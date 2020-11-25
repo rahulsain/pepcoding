360f57bd064bd86f32b274547b2ea5f43b276e27
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-25 18:57:13 +0530

    added 1.3 bar chart soln

---
+++ Online Java Foundation/1. Basics of Programming/1.3 Functions and Arrays/bar-chart-official.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        for (int j = max-1; j >= 0; j--) {
+
+            for (int i = 0; i < n; i++) {
+                if (a[i] > j) {
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
