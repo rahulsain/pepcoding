c391c0da199eb4f208286a37698831b1b2df6d2e
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-25 18:27:24 +0530

    added soln of 1.3 searching in array

---
+++ Online Java Foundation/1. Basics of Programming/1.3 Functions and Arrays/find-element-in-array-official.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+
+        // Arrays.sort(a);
+
+        System.out.println(linearSearch(a, n, key));
+    }
+
+    public static int binarySearch(int[] a, int low, int high, int k) {
+        int mid = (low + high) / 2;
+        while (low <= high) {
+            if (a[mid] == k) {
+                return mid;
+            } else if (a[mid] > k) {
+                return binarySearch(a, low, mid - 1, k);
+            } else {
+                return binarySearch(a, mid + 1, high, k);
+            }
+        }
+
+        return -1;
+    }
+    public static int linearSearch(int[] a, int n, int k) {
+        int i = 0;
+        while (i < n) {
+            if (a[i] == k) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+}
