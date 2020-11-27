d48103a1fcc3f301b02e2f6dc7f664428f9ba801
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-27 17:41:16 +0530

    added soln for 1.4 exit point matrix

---
+++ Online Java Foundation/1. Basics of Programming/1.4 2D Arrays/exit-point-matrix-official.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int a[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(br.readLine());
+            }
+        }
+        int rcur = 0;
+        int ccur = 0;
+        int dir = 0;
+
+        while (true) {
+            dir = (dir + a[rcur][ccur]) % 4;
+
+            if (dir == 0) {
+                ccur++;
+            } else if (dir == 1) {
+                rcur++;
+            } else if (dir == 2) {
+                ccur--;
+            } else {
+                rcur--;
+            }
+
+            if (ccur < 0) {
+                ccur++;
+                break;
+            } else if (rcur < 0) {
+                rcur++;
+                break;
+            } else if (ccur > cmax) {
+                ccur--;
+                break;
+            } else if (rcur > rmax) {
+                rcur--;
+                break;
+            }
+        }
+        System.out.println(rcur + "\n" + ccur);
+    }
+
+}
