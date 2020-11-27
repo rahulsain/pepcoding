b696c5601541da505ef5d36f9143dc6e47530f86
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-27 12:02:56 +0530

    added soln of 1.4 matrix multiplication

---
+++ Online Java Foundation/1. Basics of Programming/1.4 2D Arrays/matrix-multiplication-official.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n1 = Integer.parseInt(br.readLine());
+    int m1 = Integer.parseInt(br.readLine());
+    int a1[][] = new int[n1][m1];
+    for(int i = 0; i<n1; i++){
+        for(int j = 0; j<m1; j++){
+            a1[i][j] = Integer.parseInt(br.readLine());
+        }
+    }
+    
+    int n2 = Integer.parseInt(br.readLine());
+    int m2 = Integer.parseInt(br.readLine());
+    int a2[][] = new int[n2][m2];
+    for(int i = 0; i<n2; i++){
+        for(int j = 0; j<m2; j++){
+            a2[i][j] = Integer.parseInt(br.readLine());
+        }
+    }
+    if(n2 != m1){
+        System.out.println("Invalid input");
+        return;
+    }
+    
+    int a[][] = new int[n1][m2];
+    
+    for(int i = 0; i<n1; i++){
+        for(int j = 0; j<m2; j++){
+            a[i][j] = 0;
+            for(int k = 0; k<n2; k++)
+                a[i][j] += a1[i][k] * a2[k][j];
+        }
+    }
+    
+    for(int i = 0; i<n1; i++){
+        for(int j = 0; j<m2; j++){
+            System.out.print(a[i][j]+" ");
+        }
+        System.out.println();
+    }
+    
+ }
+
+}
