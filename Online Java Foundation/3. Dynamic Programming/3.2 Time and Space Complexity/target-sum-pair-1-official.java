363063810385ec35d846cd76b483f3f51a7b0245
rahulsain <50570677+rahulsain@users.noreply.github.com>
2021-09-23 21:10:29 +0530

    added soln of 3.2 tar sum pair, 2 ptr algo

---
+++ Online Java Foundation/3. Dynamic Programming/3.2 Time and Space Complexity/target-sum-pair-1-official.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void targetSumPair(int[] arr, int target){
+    //write your code here
+    
+    Arrays.sort(arr); // nlogn
+    
+    int i = 0, j = arr.length-1;
+    
+    while(i < j){
+        if(arr[i] + arr[j] == target){
+            System.out.println(arr[i] + ", " + arr[j]);
+            j--;
+            i++;
+        } else if(arr[i] + arr[j] > target){
+            j--;
+        } else {
+            i++;
+        }
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    int target = scn.nextInt();
+    targetSumPair(arr,target);
+  }
+
+}
